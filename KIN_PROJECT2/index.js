3cbd4989d3e1e71eddf8e1d6789b6968b6d087bf
Salisspro <96405129+Salisspro@users.noreply.github.com>
2024-04-27 06:02:53 +0100

    Add files via upload

---
+++ KIN_PROJECT2/index.js
@@ -0,0 +1,9 @@
+const head = document.querySelector('h1')
+
+const nameElem = document.getElementById('name')
+
+const passwordElem = document.getElementById('password')
+
+const emailElem = document.getElementById('email')
+
+console.log(head, nameElem, passwordElem, emailElem);
